90235e05439fd862e2523a28b8280d6362d098ec
AgileSoul <jegalindot@ut.edu.co>
2021-08-16 23:44:00 -0500

    Dhcp-server working now, exit function improved

--- initAccessPoint.sh
+++ initAccessPoint.sh
@@ -11,9 +11,6 @@ clear
 readonly AccessPointPath=$(dirname $(readlink -f $0))
 readonly AccessPointLibPath="$AccessPointPath/lib"
 
-readonly InterfaceAccessPoint="wlan1"
-readonly InterfaceForward="wlan0" # this interface should have internet connection
-
 # Original files to save later
 readonly InterfacesOriginalFile="/etc/network/interfaces"
 readonly DhcpDefaultConfigFile="/etc/default/isc-dhcp-server"
@@ -24,16 +21,37 @@ source "$AccessPointLibPath/resolutionUtils.sh"
 source "$AccessPointLibPath/colorUtils.sh"
 source "$AccessPointLibPath/fileUtils.sh"
 
-get_network_info()
+# ========== < Configure Variables > ============
+AccessPointState="Not Ready"
+
+# Access point interface and forwarding interface
+declare -Ar AllInterfaces=( 
+  ["AccessPoint"]="wlan1" 
+  ["Forward"]="wlan0" 
+)
+
+check_interfaces_integrity()
 {
+    local interface
+    for interface in "${AllInterfaces[@]}"
+    do
+        if ! ifconfig "$interface" &> /dev/null; then
+            echo -e "$AccessPointDefaultError the interface [$Verde$interface$TerminarColor] isn't configured, please configure it"
+            return 1
+        fi
+    done
+
     local -r ipRegex='[0-9]{2,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'
 
-    readonly ForwardIp=$(ifconfig "$InterfaceForward" | grep -E "$ipRegex" | awk '{print $2}')
-    if [ ! "$ForwardIp" ]; then
-        echo -e "$AccessPointDefaultError the interface [$Verde$InterfaceForward$TerminarColor] isn't configured, please connect it to internet"
-        return 1
+    readonly ForwardIp=$(ifconfig "${AllInterfaces["Forward"]}" | grep -E "$ipRegex" | awk '{print $2}')
+    if [ ! $ForwardIp ]; then
+        echo -e "$AccessPointDefaultError the forward interface [$Verde${AllInterfaces["Forward"]}$TerminarColor] hasn't Ip, please connect it to a wifi network"
+        return 2
     fi
+}
 
+get_network_info()
+{
     local -r idVlanForward=$(echo $ForwardIp | cut -d "." -f 3 )
     
     if [  $idVlanForward -gt 244 ]; then
@@ -48,9 +66,7 @@ get_network_info()
     readonly AccessPointBroadcast="$(echo "$AccessPointIp" | cut -d "." -f 1,2,3).255"
     readonly AccessPointSubnet="${AccessPointIp%.*}.0"
     readonly AccessPointDhcpRange="${AccessPointIp%.*}.2 ${AccessPointIp%.*}.254"
-    readonly AccessPointDnsServers="8.8.8.8 8.8.4.4"
-
-    set_interfaces_config
+    readonly AccessPointDnsServer="8.8.8.8"
 }
 
 set_interfaces_config()
@@ -60,8 +76,8 @@ set_interfaces_config()
     move_file "$InterfacesOriginalFile" "$InterfacesOriginalCopyFile" set_ 
 
     local -r configuration=(
-      "auto lo" "iface lo inet loopback" "" "auto $InterfaceAccessPoint" 
-      "iface $InterfaceAccessPoint inet static" "   address $AccessPointIp" 
+      "auto lo" "iface lo inet loopback" "" "auto ${AllInterfaces["AccessPoint"]}" 
+      "iface ${AllInterfaces["AccessPoint"]} inet static" "   address $AccessPointIp" 
       "   netmask $AccessPointNetmask" "   broadcast $AccessPointBroadcast" 
     )
     
@@ -88,7 +104,7 @@ set_hostapd_config()
     # I delete "wpa_key_mgmt=WPA­-PSK" becouse i have an error starting hostapd service.
     # Attempt uncomment it if you don't have errors with "wpa_key_mgmt=WPA­-PSK"
     local -r configuration=(
-      "interface=$InterfaceAccessPoint" "driver=nl80211" 
+      "interface=${AllInterfaces["AccessPoint"]}" "driver=nl80211" 
       "ssid=$SSID" "channel=$channel" "hw_mode=g" "auth_algs=1" 
       "wpa=2" "wpa_passphrase=$password" #"wpa_key_mgmt=WPA­-PSK" 
       "wpa_pairwise=TKIP CCMP" "rsn_pairwise=CCMP" "ap_max_inactivity=21600"
@@ -123,7 +139,7 @@ set_dhcp_config()
     # Specify the access point interface for the dhcp server.
     local -r old_dhcp_interfacesv4="$(grep -E "^INTERFACESv4=\".*\"$" $DhcpDefaultConfigFile)"
     local -r new_dhcp_interfacesv4="$(echo "$old_dhcp_interfacesv4" | 
-      sed -r "s/(^INTERFACESv4=)\".*\"$/\1\"$InterfaceAccessPoint\"/")"
+      sed -r "s/(^INTERFACESv4=)\".*\"$/\1\"${AllInterfaces["AccessPoint"]}\"/")"
     
     if [ ! "$old_dhcp_interfacesv4" -o ! "$new_dhcp_interfacesv4" ]; then
         echo -e "$AccessPointDefaultError Cannot find 'INTERFACESv4' in $DhcpDefaultConfigFile"
@@ -140,15 +156,22 @@ set_dhcp_config()
     move_file "$DhcpServerConfigFile" "$DhcpServerConfigCopyFile" set_
 
     local -r dhcp_server_configuration=(
-      "default­lease­time 600;"
-      "max­lease­time 7200;"
+      "option domain-name \"example.org\";"
+      "option domain-name-servers ns1.example.org, ns2.example.org;"
+      "" 
+      "default-lease-time 600;"
+      "max-lease-time 7200;"
+      ""
+      "ddns-update-style none;"
+      ""
+      "authoritative;"
       ""
       "subnet $AccessPointSubnet netmask $AccessPointNetmask"
       "{"
       "    range $AccessPointDhcpRange;"
       "    option routers $ForwardIp;"
       "    option subnet-mask $AccessPointNetmask;"
-      "    option domain-name-servers $AccessPointDnsServers;"
+      "    option domain-name-servers $AccessPointDnsServer;"
       "}"
     )
 
@@ -172,16 +195,18 @@ unset_dhcp_config()
 
 restart_services()
 {
+    if [ "$AccessPointState" != "Ready" ]; then return 1; fi
+
     local -r SERVICESLine="$AmarilloOscuro[$PurpuraOscuro*$AmarilloOscuro]$TerminarColor"
     local -r SERVICESSuccessLine="$Amarillo[$Purpura-$Amarillo]$TerminarColor"
     local -r SERVICESFailedLine="$Amarillo[$Rojo-$Amarillo]$TerminarColor"
 
     echo -e "$SERVICESLine Restarting the services!"
 
-    if ip addr flush dev "$InterfaceAccessPoint"; then
-        echo -e "$SERVICESSuccessLine Ready to reset $Purpura$InterfaceAccessPoint$TerminarColor Ip" 
+    if ip addr flush dev "${AllInterfaces["AccessPoint"]}"; then
+        echo -e "$SERVICESSuccessLine Ready to reset $Purpura${AllInterfaces["AccessPoint"]}$TerminarColor Ip" 
     else
-        echo -e "$SERVICESFailedLine Cannot delete current $Purpura$InterfaceAccessPoint$TerminarColor Ip. Error (${Purpura}2$TerminarColor)"
+        echo -e "$SERVICESFailedLine Cannot delete current $Purpura${AllInterfaces["AccessPoint"]}$TerminarColor Ip. Error (${Purpura}2$TerminarColor)"
         return 1
     fi
 
@@ -199,7 +224,7 @@ restart_services()
         return 3
     fi
 
-    echo
+    AccessPointState="Not Ready"
 }
 
 start_access_point()
@@ -220,7 +245,7 @@ stop_access_point()
     
     systemctl stop hostapd &> /dev/null
     systemctl disable hostapd &> /dev/null
-    kill $AccessPointPID
+    kill $AccessPointPID 2> /dev/null
 }
 
 start_dhcp_server()
@@ -241,27 +266,32 @@ stop_dhcp_server()
 
     systemctl stop isc-dhcp-server &> /dev/null
     systemctl disable isc-dhcp-server &> /dev/null
-    kill $DhcpServerPID
+    kill $DhcpServerPID 2> /dev/null
 }
+
+readonly RESTORESuccessLine="$Gris[$Cyan-$Gris]$TerminarColor"
+readonly RESTOREFailedLine="$Gris[$Rojo-$Gris]$TerminarColor"
 
 restore_original_files()
 {
+    if [ "$AccessPointState" == "Not Ready" ]; then return 1; fi
+
     local -r RESTORELine="$GrisOscuro[$CyanOscuro*$GrisOscuro]$TerminarColor"
-    local -r RESTORESuccessLine="$Gris[$Cyan-$Gris]$TerminarColor"
-    local -r RESTOREFailedLine="$Gris[$Rojo-$Gris]$TerminarColor"
 
     echo -e "$RESTORELine Restoring original files!"
     
     unset_interfaces_config
     unset_hostapd_config
     unset_dhcp_config
-    
-    echo
+
+    # This activate restart_services function
+    AccessPointState="Ready"
 }
 
 stop_servers()
 {
-    local servers_counter=0
+    if [ "$AccessPointState" != "Running" ]; then return 1; fi
+    
     local -r SERVERSLine="$AzulOscuro[$AmarilloOscuro*$AzulOscuro]$TerminarColor"
     local -r SERVERSSuccessLine="$Azul[$Amarillo-$Azul]$TerminarColor"
     local -r SERVERSFailedLine="$Azul[$Rojo-$Azul]$TerminarColor"
@@ -270,73 +300,122 @@ stop_servers()
 
     if stop_access_point; then
         echo -e "$SERVERSSuccessLine Stop access point \"$SSID\" ${AmarilloOscuro}Successfully$TerminarColor"
-        let servers_counter++
     fi
 
     if stop_dhcp_server; then
         echo -e "$SERVERSSuccessLine Stop dhcp server ${AmarilloOscuro}Successfully$TerminarColor"
-        let servers_counter++
     fi
 
-    if [ $servers_counter -eq 0 ]; then
-        echo -e "$SERVERSFailedLine There aren't servers to stop"
+    # Stop spinner
+    kill "$spinnerPID" 2> /dev/null 
+
+    AccessPointState="Ready"
+}
+
+set_access_point_config()
+{
+    if check_interfaces_integrity; then 
+        get_network_info
+        set_interfaces_config
+    else
+        return 1;
     fi
 
-    echo
-}
+    if ! set_hostapd_config; then 
+        unset_hostapd_config
+        return 2
+    fi
 
-exit_script()
-{
-    restore_original_files
-
-    stop_servers
-
-    echo -e "$AccessPointVLine Exiting..."; sleep 0.2
-
-    exit $1
-}
-
-prep_access_point()
-{
-    if ! get_network_info; then return 1; fi
-
-    if ! set_hostapd_config; then return 2; fi
-
-    if ! set_dhcp_config; then return 3; fi
+    if ! set_dhcp_config; then 
+        unset_dhcp_config
+        return 3
+    fi
 }
 
 run_access_point()
 {
     # Attempt restarting services
     if ! restart_services; then 
-        echo -e "Failed to restarting the services [Error $Rojo$?$TerminarColor], Exiting..."
+        echo -e "Failed to restarting the services, Exiting...\n"
         return 1
     fi
-
+    echo
     if ! start_access_point; then
-        echo - "Failed to start access point [Error $Rojo$?$TerminarColor], Exiting..."
+        echo - "Failed to start access point, Exiting..."
         return 2
     fi
 
     if ! start_dhcp_server; then
-        echo -e "Failed to start dhcp server [Error $Rojo$?$TerminarColor], Exiting..."
+        echo -e "Failed to start dhcp server, Exiting..."
         return 3
     fi
+}
 
-    sleep 20
+access_point_running_spinner()
+{
+    local -r charSequence="-|+|*|/|\\|"
+    printf '     '
+    while true;
+    do
+    	for (( i=0; i<${#charSequence}; i++))
+    	do
+            sleep 0.6
+            #printf "\b$RojoOscuro%s$VerdeOscuro%s$RojoOscuro%s$TerminarColor" "[ " "${charSequence:$i:1}" " ]"
+    	    #printf "\b$RojoOscuro[ $VerdeOscuro${charSequence:i:1} $RojoOscuro]"
+            echo -ne "\r\t\t$RojoOscuro[ $VerdeOscuro${charSequence:i:1} $RojoOscuro]"
+        done 
+    done
+}
+
+exit_script()
+{
+    echo -e "\n$AccessPointLine Exiting..."
+
+    stop_servers
+    sleep 0.9
+
+    restore_original_files
+    sleep 0.9
+
+    restart_services
+    sleep 0.9
+
+    exit $1
 }
 
 access_point_main()
 {
-    readonly AccessPointVLine="$RojoOscuro[$VerdeOscuro*$RojoOscuro]$TerminarColor"
+    readonly AccessPointLine="$RojoOscuro[$VerdeOscuro*$RojoOscuro]$TerminarColor"
     readonly AccessPointDefaultError="${RojoOscuro}Error:$TerminarColor"
     
     set_resolution
 
-    if ! prep_access_point; then return 1; fi
-    if ! run_access_point; then return 2; fi
+    if set_access_point_config; then 
+        AccessPointState="Ready"
+    else
+        return 1
+    fi
 
-    exit_script 0
+    if run_access_point; then
+        AccessPointState="Running"
+    else
+        return 2
+    fi
+    
+    echo -e "\t$AccessPointLine Access Point '$SSID' is ${Verde}running$TerminarColor $AccessPointLine"
+    
+    echo -e "\tPress [Ctrl + c] to shutdown the access point"
+    access_point_running_spinner &
+    local -r spinnerPID=$!
+    
+    while true
+    do
+        sleep 10
+    done
 }
 
-if ! access_point_main; then exit_script $?; fi
+trap exit_script "0" SIGINT SIGHUP
+
+access_point_main 
+exit_script $?
+
